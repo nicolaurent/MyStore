e464784072c2c5ebdae4ca81a8f2270d87eef359
nicolaurent <nico.laurent1@gmail.com>
2021-08-10 23:29:19 +0800

    Update development

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { CartComponent } from './component/cart/cart.component';
+import { ConfirmationComponent } from './component/confirmation/confirmation.component';
+import { ProductItemComponent } from './component/product-item/product-item.component';
+import { ProductItemDetailComponent } from './component/product-item-detail/product-item-detail.component';
+import { ProductListComponent } from './component/product-list/product-list.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CartComponent,
+    ConfirmationComponent,
+    ProductItemComponent,
+    ProductItemDetailComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/product-list.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import datajson from '../../assets/data.json';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductListService {
+
+  constructor(private http: HttpClient) { }
+
+  getProductList(): Observable<[]> {
+    return this.http.get(datajson);
+  }
+}
