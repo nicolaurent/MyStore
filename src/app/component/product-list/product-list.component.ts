5df2ea9da5d1f87964edd55c8f65536437fc8276
nicolaurent <nico.laurent1@gmail.com>
2021-08-15 23:22:45 +0800

    Update reading from json file

--- src/app/component/product-list/product-list.component.ts
+++ src/app/component/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductListService, dataJson } from 'src/app/services/product-list.service';
+
 
 @Component({
   selector: 'app-product-list',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  productList: dataJson[] = []
 
-  constructor() { }
+  constructor(private productListService: ProductListService) { }
 
   ngOnInit(): void {
+    this.productListService.getProductList().subscribe(res => {
+      this.productList = res
+    });
+
+    
   }
 
 }
--- src/app/services/product-list.service.ts
+++ src/app/services/product-list.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-// import { Observable } from 'rxjs';
-// import datajson from '../../assets/data.json';
+import { Observable } from 'rxjs';
+import datajson from '../../assets/data.json';
+
+export type dataJson = {
+  id: number, name: string, price: number, url: string, description: string
+}
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +14,9 @@ export class ProductListService {
 
   constructor(private http: HttpClient) { }
 
-  /*
-  getProductList(): Observable<[]> {
-    return this.http.get(datajson);
+
+  getProductList():Observable<[]>{
+    return this.http.get<[]>('../../assets/data.json');
   }
-  */
+
 }
